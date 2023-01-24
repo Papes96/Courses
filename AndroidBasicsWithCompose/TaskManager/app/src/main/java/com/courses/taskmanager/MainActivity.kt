f49a1cc565018817ba58404fa2421291c65d69e8
Papes <javier.papes@gmail.com>
2023-01-24 16:46:46 -0300

    Task Manager: Unit 1 - Pathway 3 - Activity 5

---
+++ AndroidBasicsWithCompose/TaskManager/app/src/main/java/com/courses/taskmanager/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.courses.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.material3.FabPosition.Companion.Center
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.courses.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ImageMessageScreen(
+                        painterResource(R.drawable.ic_task_completed),
+                        stringResource(R.string.title),
+                        stringResource(R.string.message)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HeaderImage(image: Painter) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun Title(title: String) {
+    Text(
+        text = title,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(0.dp, 24.dp, 0.dp, 8.dp)
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun Message(message: String) {
+    Text(
+        text = message,
+        textAlign = TextAlign.Center,
+        fontSize = 16.sp,
+        modifier = Modifier
+            .padding(16.dp, 0.dp, 16.dp, 16.dp)
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun ImageMessageScreen(image: Painter, title: String, message: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .wrapContentHeight(Alignment.CenterVertically, false)
+    ) {
+        HeaderImage(image)
+        Title(title)
+        Message(message)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TaskManagerTheme {
+        ImageMessageScreen(
+            painterResource(R.drawable.ic_task_completed),
+            stringResource(R.string.title),
+            stringResource(R.string.message)
+        )
+    }
+}
